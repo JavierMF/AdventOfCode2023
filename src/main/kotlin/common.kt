4a64c024d800fe11ce1ea0882ab97407caafde0e
Javier Muñoz <javier.munozferrara@vonage.com>
2023-12-01 18:21:11 +0100

    Project settings + Solution for Day 1

---
+++ src/main/kotlin/common.kt
@@ -0,0 +1,40 @@
+import java.io.File // ktlint-disable filename
+import kotlin.system.exitProcess
+
+fun getFileFromArgs(args: Array<String>): File {
+    if (args.isEmpty()) {
+        println("Input file path required")
+        exitProcess(-1)
+    }
+    return getFileFromFilePath(args.first())
+}
+
+fun getFileFromFilePath(filePath: String): File {
+    val file = File(filePath)
+    if (!file.exists()) {
+        println("Input file does not exist")
+        exitProcess(-1)
+    }
+    return file
+}
+
+fun getNonBlankFileLines(args: Array<String>) =
+    getFileFromArgs(args)
+        .readLines()
+        .filter { it.isNotBlank() }
+
+data class Coords(val x: Int, val y: Int) {
+
+    fun neighbours(maxX: Int, maxY: Int): Set<Coords> = range(x, maxX)
+        .flatMap { posX ->
+            range(y, maxY).map { posY -> Coords(posX, posY) }
+                .filter { it != this }
+        }.toSet()
+
+    private fun range(i: Int, max: Int): Set<Int> = (i - 1..i + 1)
+        .filter { it in 0..max }.toSet()
+}
+
+data class Grid(
+    val coords: Set<Coords>,
+)
